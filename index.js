444b4fc63db7d37aa1d765bf09a8a7b16ef93b39
Amel252 <amelghanem25@gmail.com>
2025-10-27 02:13:39 +0100

    installation de handlebars

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
 const path = require("path");
 const PORT = process.env.PORT || 5003;
 
@@ -6,6 +7,9 @@ const app = express();
 
 //middleware(tou ce qui se passe au milieu )
 app.use(express.static(path.join(__dirname, "public"))); // il va considérer comme racine du projet et il va l'afficher
+//  je vais appelé handlebars qui vient de exphbs
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
