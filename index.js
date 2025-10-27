1eddae820d35ab8c1bad133eef003b494b9cf2be
Amel252 <amelghanem25@gmail.com>
2025-10-27 01:59:43 +0100

    Faire middleware pour afficher notre page html

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const path = require("path");
 const PORT = process.env.PORT || 5003;
 
 const app = express();
+
+//middleware(tou ce qui se passe au milieu )
+app.use(express.static(path.join(__dirname, "public"))); // il va considérer comme racine du projet et il va l'afficher
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
