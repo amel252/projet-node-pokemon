ad008668c323debfd3c27468ae574524085d2c3b
Amel252 <amelghanem25@gmail.com>
2025-10-29 00:01:32 +0100

    maper la data et affichr notre liste pokemon (page home)

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const { engine } = require("express-handlebars");
+// const fetch = require("node-fetch");
+const fetch = (...args) =>
+    import("node-fetch").then(({ default: fetch }) => fetch(...args));
+
 // const exphbs = require("express-handlebars");
 const path = require("path");
 const PORT = process.env.PORT || 5003;
 
 const app = express();
-
+//  récuperer la data
+const getAllPokemon = async () => {
+    try {
+        const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151");
+        const json = await res.json();
+        console.table(json.results);
+        return json;
+    } catch (err) {
+        console.log(err);
+    }
+};
 //middleware(tou ce qui se passe au milieu )
 app.use(express.static(path.join(__dirname, "public"))); // il va considérer comme racine du projet et il va l'afficher
 //  je vais appelé handlebars qui vient de exphbs
@@ -13,8 +27,14 @@ app.engine(".hbs", engine({ extname: ".hbs" }));
 app.set("view engine", ".hbs");
 
 // routes
-app.get("/", (req, res) => {
-    res.render("home", { title: "Home" });
+app.get("/", async (req, res) => {
+    try {
+        const pokemons = await getAllPokemon();
+        res.render("home", { pokemons });
+    } catch (err) {
+        console.log(err);
+        // res.status(500).send("Erreur serveur");
+    }
 });
 // ca nous evite de créer une page pour chaque pokémon
 app.get("/:title", (req, res) => {
