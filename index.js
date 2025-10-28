9305861a7c0d67df50cbf2fdbe7d2b6a48aa30d1
Amel252 <amelghanem25@gmail.com>
2025-10-28 12:23:39 +0100

    Config projet avec handlebars

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const exphbs = require("express-handlebars");
+const { engine } = require("express-handlebars");
+// const exphbs = require("express-handlebars");
 const path = require("path");
 const PORT = process.env.PORT || 5003;
 
@@ -8,8 +9,12 @@ const app = express();
 //middleware(tou ce qui se passe au milieu )
 app.use(express.static(path.join(__dirname, "public"))); // il va considérer comme racine du projet et il va l'afficher
 //  je vais appelé handlebars qui vient de exphbs
-app.engine("handlebars", exphbs());
+app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
+
+app.get("/", (req, res) => {
+    res.render("home", { title: "Home" });
+});
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
