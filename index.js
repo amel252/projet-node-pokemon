2df1e73925a0dc7379b8c2d2f6095eb964ad208b
Amel252 <amelghanem25@gmail.com>
2025-10-28 23:08:53 +0100

    Configuration handlebars par .hbs

--- index.js
+++ index.js
@@ -9,15 +9,13 @@ const app = express();
 //middleware(tou ce qui se passe au milieu )
 app.use(express.static(path.join(__dirname, "public"))); // il va considérer comme racine du projet et il va l'afficher
 //  je vais appelé handlebars qui vient de exphbs
-app.engine("handlebars", engine());
-app.set("view engine", "handlebars");
+app.engine(".hbs", engine({ extname: ".hbs" }));
+app.set("view engine", ".hbs");
 
+// routes
 app.get("/", (req, res) => {
     res.render("home", { title: "Home" });
 });
-// app.get("/about", (req, res) => {
-//     res.render("about", { title: "about", subTitle: "Ma about page" });
-// });
 // ca nous evite de créer une page pour chaque pokémon
 app.get("/:title", (req, res) => {
     const title = req.params.title;
